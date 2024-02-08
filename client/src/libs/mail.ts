36f60a0b434f276bda2dc5f08d833f4bcbec81c1
SWitsarut <witsarut1133@outlook.com>
2024-02-08 18:03:58 +0700

    add env

--- client/src/libs/mail.ts
+++ client/src/libs/mail.ts
@@ -3,12 +3,12 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(email: string, token: string) {
-  const confirmLink = `${process.env.NEXT_PUBLIC_URL}/new-verification?token=${token}`
+	const confirmLink = `${process.env.NEXT_PUBLIC_URL}/new-verification?token=${token}`;
 
-  await resend.emails.send({
-    from: "e-book-store.noreply <onboarding@resend.dev>",
-    to: email,
-    subject: "Confirm your email",
-    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
-  })
+	await resend.emails.send({
+		from: "e-book-store.noreply <onboarding@resend.dev>",
+		to: email,
+		subject: "Confirm your email",
+		html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
+	});
 }
--- client/src/theme/index.ts
+++ client/src/theme/index.ts
@@ -1,35 +1,29 @@
-import {
-  MantineColorsTuple,
-  createTheme,
-  DefaultMantineColor,
-} from "@mantine/core";
+import { MantineColorsTuple, createTheme, DefaultMantineColor } from "@mantine/core";
 
-type ExtendedCustomColors =
-  | "primary"
-  | DefaultMantineColor;
+type ExtendedCustomColors = "primary" | DefaultMantineColor;
 
 declare module "@mantine/core" {
-  export interface MantineThemeColorsOverride {
-    colors: Record<ExtendedCustomColors, MantineColorsTuple>;
-  }
+	export interface MantineThemeColorsOverride {
+		colors: Record<ExtendedCustomColors, MantineColorsTuple>;
+	}
 }
 
 const primary: MantineColorsTuple = [
-  "#e5f4ff",
-  "#cde2ff",
-  "#9bc2ff",
-  "#64a0ff",
-  "#3984fe",
-  "#1d72fe",
-  "#0969ff",
-  "#0058e4",
-  "#004ecc",
-  "#0043b5"
+	"#e5f4ff",
+	"#cde2ff",
+	"#9bc2ff",
+	"#64a0ff",
+	"#3984fe",
+	"#1d72fe",
+	"#0969ff",
+	"#0058e4",
+	"#004ecc",
+	"#0043b5",
 ];
 
 export const theme = createTheme({
-  primaryColor: "primary",
-  colors: {
-    primary,
-  }
-})
+	primaryColor: "primary",
+	colors: {
+		primary,
+	},
+});
