284e47fdf34c0c1ac4b09e7550ccee6beaecacff
Your Name <you@example.com>
2024-02-24 18:13:04 +0700

    fix query mistake and Fuse is now unemployed

--- client/src/app/api/publisher/manage-book/route.ts
+++ client/src/app/api/publisher/manage-book/route.ts
@@ -38,7 +38,7 @@ export const POST = async (req: Request) => {
   try {
     const publisher = await prisma.publisher.findFirst({
       where: {
-        users: {
+        manager: {
           some: {
             username: session.user.username
           }
