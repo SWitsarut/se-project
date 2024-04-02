dece55b10e1301637223e7ba18980ac3d700251e
FuseSN2003 <sukavit.fuse2003@gmail.com>
2024-04-02 19:27:02 +0700

    improve ui

--- client/src/components/Chat/MessageContent.tsx
+++ client/src/components/Chat/MessageContent.tsx
@@ -19,17 +19,17 @@ export default function MessageContent({ messages, userId }: MessageContentProps
       ) : (
         messages.map((message, index: number) => (
           <Fragment key={index}>
-            <div className={`flex flex-col max-w-[70%] gap-1 ${message.sender.id === userId ? "self-end" : "self-start"}`}>
+            <div className={`flex flex-col w-full max-w-[70%] gap-1 ${message.sender.id === userId ? "self-end" : "self-start"}`}>
               <div className={`flex items-center gap-2 ${message.sender.id === userId ? "flex-row-reverse" : "justify-start"}`}>
                 <div className={`flex gap-1 items-center ${message.sender.id === userId ? "flex-row-reverse" : "justify-start"}`}>
                   <Avatar size="sm" src={message.sender.id === userId ? message.sender.avatar : null} alt={message.sender.username} />
                   <Text size="xs">{message.sender.id === userId ? "You" : "Admin" }</Text>
                 </div>
-                <div>
+                <div className="flex items-center">
                   <Text c="dimmed" classNames={{ root: "text-[10px]" }}>{new Date(message.timeStamp).toLocaleTimeString()}</Text>
                 </div>
               </div>
-              <div className={`break-words text-wrap border p-2 rounded-lg ${message.sender.id === userId ? "bg-primary text-white self-end" : "bg-white text-black self-start" }`}>
+              <div className={`break-words text-wrap border p-2 rounded-lg ${message.sender.id === userId ? "bg-primary text-white" : "bg-white text-black" }`}>
                 <Text>{message.content}</Text>
               </div>
             </div>
